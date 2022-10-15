f5cb7b44ddb5f2da3fedd77768d48e463f1283c5
Abdulsamet Aybaz <aaybaz@k2m16s06.42kocaeli.com.tr>
2022-10-15 18:38:08 +0300

    18 commit

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: aaybaz <aaybaz@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/06 14:16:22 by aaybaz            #+#    #+#             */
-/*   Updated: 2022/10/15 18:23:31 by aaybaz           ###   ########.fr       */
+/*   Updated: 2022/10/15 18:37:27 by aaybaz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,7 @@ void				*ft_memchr(const void *s, int c, size_t n);
 int					ft_memcmp(const void *s1, const void *s2, size_t n);
 char				*ft_strnstr(const char *str, const char *find_substr,
 						size_t len);
-int					ft_atoi(const char *str);					
+int					ft_atoi(const char *str);		
 void				*ft_calloc(size_t count, size_t size);
 char				*ft_strdup(const char *str);
 
